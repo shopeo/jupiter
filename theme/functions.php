c0ea0d429040943a04f375e6e19059a887b8efa8
jabber <2898117012@qq.com>
2022-07-10 01:25:43 +0800

    init

--- theme/functions.php
+++ theme/functions.php
@@ -3,11 +3,55 @@ if ( ! function_exists( 'uranus_support' ) ) {
 	function uranus_support() {
 		add_theme_support( 'automatic-feed-links' );
 
+		add_theme_support( 'custom-background', array(
+			'default-color' => 'F5EFE0',
+		) );
+
+		global $content_width;
+		if ( ! isset( $content_width ) ) {
+			$content_width = 640;
+		}
+
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 1200, 9999 );
+		add_image_size( 'uranus-fullscreen', 1980, 9999 );
+
+		$logo_width  = 120;
+		$logo_height = 90;
+
+		if ( get_theme_mod( 'retina_logo', false ) ) {
+			$logo_width  = floor( $logo_width * 2 );
+			$logo_height = floor( $logo_height * 2 );
+		}
+
+		add_theme_support( 'custom-logo', array(
+			'height'      => $logo_height,
+			'width'       => $logo_width,
+			'flex-height' => true,
+			'flex-width'  => true,
+		) );
+
 		add_theme_support( 'title-tag' );
+
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'script',
+				'style',
+				'navigation-widgets',
+			)
+		);
+
+		load_theme_textdomain( 'uranus' );
+
 		add_theme_support( 'align-wide' );
 		add_theme_support( 'responsive-embeds' );
-		
-		load_theme_textdomain( 'uranus' );
+
 	}
 }
 
@@ -28,7 +72,7 @@ add_action( 'wp_enqueue_scripts', 'uranus_styles' );
 if ( ! function_exists( 'uranus_scripts' ) ) {
 	function uranus_scripts() {
 		$theme_version = wp_get_theme()->get( 'Version' );
-		wp_enqueue_script( 'uranus-script', get_template_directory_uri() . '/assets/js/app.js', array(), $theme_version );
+		wp_enqueue_script( 'uranus-script', get_template_directory_uri() . '/assets/js/app.js', array( 'jquery' ), $theme_version );
 		wp_script_add_data( 'uranus-script', 'async', true );
 	}
 }
